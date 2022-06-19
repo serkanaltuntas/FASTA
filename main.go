ff5348fe777393a121b1c045bfbf2d913c3eb617
Serkan Altuntaş <serkanaltuntas@protonmail.com>
2022-06-19 17:13:58 +0300

    Reorder function declerations.

--- main.go
+++ main.go
@@ -12,6 +12,37 @@ type fasta struct {
 	sequence string
 }
 
+// create main fucntion and call readFasta function
+func main() {
+	// get the filename from the command line	and store it in a variable if file is not evailable warn the user	and exit
+	if len(os.Args) != 2 {
+		println("Usage:", os.Args[0], "filename")
+		os.Exit(1)
+	}
+	filename := os.Args[1]
+	records, err := readFasta(filename)
+	if err != nil {
+		panic(err)
+	}
+	for _, record := range records {
+		fmt.Println(record.header)
+		fmt.Println(record.sequence)
+
+		// if sequence include characters other than A,a,T,t,C,c,G,g, warn the user and pass to the next record
+		if !(record.sequence[0] == 'A' || record.sequence[0] == 'a' || record.sequence[0] == 'T' || record.sequence[0] == 't' || record.sequence[0] == 'C' || record.sequence[0] == 'c' || record.sequence[0] == 'G' || record.sequence[0] == 'g') {
+			fmt.Println("Warning: sequence contains characters other than A,a,T,t,C,c,G,g, skipping record for GC content calculation.")
+			continue
+		}
+
+		// calculate GC content and print it
+		gc, at, nt := record.GCcontent()
+		fmt.Println("-----Summary-----")
+		fmt.Println("All:", nt)
+		fmt.Printf("GC content: %.2f\n", gc)
+		fmt.Printf("AT content: %.2f\n", at)
+	}
+}
+
 // read fasta file and save its content as fasta records	in a slice
 func readFasta(filename string) ([]fasta, error) {
 	// open file
@@ -43,37 +74,6 @@ func readFasta(filename string) ([]fasta, error) {
 
 	return records, nil
 
-}
-
-// create main fucntion and call readFasta function
-func main() {
-	// get the filename from the command line	and store it in a variable if file is not evailable warn the user	and exit
-	if len(os.Args) != 2 {
-		println("Usage:", os.Args[0], "filename")
-		os.Exit(1)
-	}
-	filename := os.Args[1]
-	records, err := readFasta(filename)
-	if err != nil {
-		panic(err)
-	}
-	for _, record := range records {
-		fmt.Println(record.header)
-		fmt.Println(record.sequence)
-
-		// if sequence include characters other than A,a,T,t,C,c,G,g, warn the user and pass to the next record
-		if !(record.sequence[0] == 'A' || record.sequence[0] == 'a' || record.sequence[0] == 'T' || record.sequence[0] == 't' || record.sequence[0] == 'C' || record.sequence[0] == 'c' || record.sequence[0] == 'G' || record.sequence[0] == 'g') {
-			fmt.Println("Warning: sequence contains characters other than A,a,T,t,C,c,G,g, skipping record for GC content calculation.")
-			continue
-		}
-
-		// calculate GC content and print it
-		gc, at, nt := record.GCcontent()
-		fmt.Println("-----Summary-----")
-		fmt.Println("All:", nt)
-		fmt.Printf("GC content: %.2f\n", gc)
-		fmt.Printf("AT content: %.2f\n", at)
-	}
 }
 
 // create a function that calculates number of G and C contents separately in a fasta record
